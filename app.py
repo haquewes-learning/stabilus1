7bfd11a4c0ebba7d62825c77e14d4f102f75fb7c
haquewes-learning <haquewes.learning@gmail.com>
2026-04-04 18:00:16 +0000

    Fixes1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import streamlit as st
+import pandas as pd  # type: ignore[import]
+import streamlit as st  # type: ignore[import]
 
 st.set_page_config(page_title="Stabilus Gas Spring Finder", layout="wide")
 
